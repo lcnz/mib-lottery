69654bb19596bbd946c2459d9d77fdd4c92d22b1
Akraier <diodatovincenzo@yahoo.com>
2021-12-09 21:45:48 +0100

    clean task code

--- background.py
+++ background.py
@@ -1,12 +1,3 @@
-#from mib import create_app, create_celery
-#
-#flask_app = create_app()
-#app = create_celery(flask_app)
-#
-#try:
-#    import mib.tasks
-#except ImportError:
-#    raise RuntimeError('Cannot import celery tasks')
 from datetime import date, datetime, timedelta
 from celery import Celery
 from celery.schedules import crontab
@@ -22,8 +13,7 @@ APP = None
 
 @celery.on_after_configure.connect
 def setup_periodic_task(sender, **kwargs):
-    #sender.add_periodic_task(crontab(hour=12, minute = 0, day_of_month=27), lottery.s(), name = 'lottery extraction')
-    sender.add_periodic_task(timedelta(seconds = 60), lottery.s(), name = 'lottery extraction')
+    sender.add_periodic_task(crontab(hour=12, minute = 0, day_of_month=27), lottery.s(), name = 'lottery extraction')
 
 # task fo the lottery
 @celery.task
@@ -35,38 +25,16 @@ def lottery():
     with APP.app_context():
         from mib import create_app
         from mib.dao.lottery_manager import LotteryManager
-        #from sqlalchemy import update
 
-        #winner = random.randrange(1,100)#exctract a random number in [1,99]
-        winner = 10
-        print("lucky number -----", winner)
+        winner = random.randrange(1,100)#exctract a random number in [1,99]
+        
         players = LotteryManager.retrieve_active_players()     #users who play the lottery
         if not players:
             return []
         for p in players:
-            print("loooooop")
-            #LotteryManager.check_winner(p) #
             if p.ticket_number == winner:      # if player's number is the extracted number, then he wins
-                print(p.points)
                 p.add_points(5)                   #winner user gains 5 points
-                
-                """Background task to send an email with Flask-Mail."""
-                
-                """subject = "Monthly Lottery Result"
-                body = "you have earned 5 points"
-                to_email = p.email
-                receiver_id = p.id
-                
-                email_data = {
-                    'subject': subject,
-                    'to': to_email,
-                    'body': body
-                }
-                msg = Message(email_data['subject'], sender=app.config['MAIL_DEFAULT_SENDER'],recipients=[email_data['to']])
-                msg.body = email_data['body']
-                mail.send(msg)"""
             p.unset_ticket_number()        #restore default value for every player at the end of lottery extraction
-            print(p.points)
             LotteryManager.update_lottery_row(p)  
         return []  
     
